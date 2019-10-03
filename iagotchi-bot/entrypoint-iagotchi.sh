316df3f477a90b218e29b9003447da05e47c9c53
LALEYE FREJUS <frejus.laleye@cea.fr>
2019-10-03 18:33:12 +0200

    Can generate sentence embeddings

--- iagotchi-bot/entrypoint-iagotchi.sh
+++ iagotchi-bot/entrypoint-iagotchi.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-# MODELFILE="data/embeddings/rencontre.bin"
-# if [ ! -f "$MODELFILE" ]; then
-#    python3 similarity.py
-# fi  
-# case "$3" in "iagotchi")
-#     cd ChatScript/BINARIES/
-#     ./LinuxChatScript64 &
-#     cd ../../
-#     python3 test_chatscript.py --bot iagotchi & python3 main.py --runbot iagotchi
-#     ;;
-#     "iagotchig5 ")
-#     cd ChatScript/BINARIES/
-#     ./LinuxChatScript64 &
-#     cd ../../
-#     python3 test_chatscript.py --bot iagotchig5 & python3 main.py --runbot iagotchig5
-#     ;;    
-#     "build_similarity")
-#     python3 main.py --build similarity
-#     ;;
-#     *)
-#     echo "You have failed to specify what to do correctly."
-#     exit 1
-#     ;;
-# esac
+exec_var=$( cat data/config.json | jq -r '.what_run' )
 
-cd ChatScript/BINARIES/
-./LinuxChatScript64 &
-cd ../../
-python3 test_chatscript.py & python3 main.py
+echo $exec_var
+
+ case $exec_var in "bot")
+      cd ChatScript/BINARIES/
+      ./LinuxChatScript64 &
+      cd ../../
+      python3 test_chatscript.py & python3 main.py
+     ;;
+     "similarity")
+     python3 similarity.py
+     ;;
+     "similarité")
+      python3 similarity.py
+     ;;
+     *)
+     echo "You have failed to specify what to do correctly in data/config.json."
+     exit 1
+     ;;
+ esac
+
