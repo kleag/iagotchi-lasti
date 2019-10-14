6ee2164b1e1be606d55807f053bb82ed4034bc9a
frejus <frejus.laleye@gmail.com>
2019-10-14 06:38:39 +0200

    unnecessary files partially deleted

--- iagotchi-bot/test_lima.py
+++
@@ -1,6 +0,0 @@
-from lima import Lima
-
-
-lima = Lima()
-
-print(lima.text_lima_tagger("c'est la vie"))
--- iagotchi-bot/test_synthese.py
+++
@@ -1,8 +0,0 @@
-from synthese import Synthese
-
-
-syn = Synthese()
-
-
-texte = "oui oui j'ai toujours mal"
-syn.synthese(texte)
